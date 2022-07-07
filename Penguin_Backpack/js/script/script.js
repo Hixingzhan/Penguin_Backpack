934b268dbc34a5bfd94e6b23cdfe6cf5d66080c8
Hixingzhan <106829085+Hixingzhan@users.noreply.github.com>
2022-07-07 20:53:02 +0800

    Create script.js

--- Penguin_Backpack/js/script/script.js
+++ Penguin_Backpack/js/script/script.js
@@ -570,6 +570,14 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
                 }
             })
             $("html,body").css({"width":"100%","height":"100%","background":"rgb(233,227,214)"});
+            $("head").html(`
+                <title>`+data.name+`</title>
+                <meta charset="utf-8" />  <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+                <script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js"></script>
+                <script src="https://cdn.staticfile.org/popper.js/1.15.0/umd/popper.min.js"></script>
+                <script src="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+            `)
             $("body").html(`
                 <div id="bookcase">
                     <div id="book">
@@ -593,8 +601,8 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
                 if(is_add) txt.push(item.replace(/\s+/g,""));
             })
             txt.map(function(item){
-                var p = document.createElement('p');
-                p.innerText = item;
+                var p = document.createElement('div');
+                p.innerHTML = item;
                 $("#text").append(p);
             })
         }
